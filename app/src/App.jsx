98e9d2c7db7c6c5f918ae76c6493bcf8d42ee460
Ingrid Lemos <ingridlc42@gmail.com>
2025-06-18 20:03:56 -0300

    create main

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,11 @@
-
 import Header from './components/header'
-
+import Main from './components/mainSection'
 
 const App = () => {
   return(
     <>
     <Header/>
+    <Main/>
     </>
   )
 }
---
+++ app/src/components/mainSection/index.jsx
@@ -0,0 +1,18 @@
+import illustrationMain from '../../assets/image/ReactJs.png'
+import './style.css'
+
+const Main = () => {
+    return(
+        <main>
+            <img src={illustrationMain} alt="Logo React" loading='lazy' />
+            <h2>Aprenda com projetos reais</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione illo et modi dignissimos blanditiis necessitatibus itaque possimus similique rem vel. Suscipit expedita iusto perferendis ut aliquam magni recusandae, tenetur quia.
+                Facilis, officia incidunt molestiae voluptate, dolorem libero porro deleniti deserunt nobis, molestias odit quae. Qui quisquam unde laudantium facere inventore, ad excepturi, ex sequi porro reprehenderit praesentium asperiores amet minus.
+                Quod nisi, nihil accusamus distinctio quam minima dolore fuga inventore quaerat obcaecati aliquam! Dignissimos quisquam est ea? Ipsa distinctio qui quam! Iste sint, nobis velit voluptatibus pariatur quod itaque totam.
+            </p>
+        </main>
+    )
+}
+
+export default Main
