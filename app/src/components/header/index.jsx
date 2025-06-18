78987d683ef5fa5dc733978b8e5dc8a116d7d038
Ingrid Lemos <ingridlc42@gmail.com>
2025-06-18 19:12:15 -0300

    first commit

---
+++ app/src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+const Header = () => {
+    return(
+        <header>
+            <h1>Aprendendo ReactJS 🚀</h1>
+            <p>Desenvolva suas habilidades de forma moderna e prática</p>
+        </header>
+    )
+}
+
+export default Header
