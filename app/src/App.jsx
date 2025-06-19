fe637b286abb859e8c34cb282a65c9b185c0f92d
Ingrid Lemos <ingridlc42@gmail.com>
2025-06-18 21:49:48 -0300

    feat: create footer end mainSection components

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './components/footer'
 import Header from './components/header'
 import Main from './components/mainSection'
 
@@ -6,6 +7,7 @@ const App = () => {
     <>
     <Header/>
     <Main/>
+    <Footer/>
     </>
   )
 }
---
+++ app/src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+const Footer = () => {
+    return(
+        <footer>
+            <p>Desenvolvido por Ingrid Lemos. Todos os direitos reservados</p>
+        </footer>
+    )
+}
+
+export default Footer
